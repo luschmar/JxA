8ef8d494a898935168cfdd7b569a127ee11a1757
luschmar <90399580+luschmar@users.noreply.github.com>
2025-08-19 22:05:01 +0200

    🚧 WIP

--- jxa-auth-server/src/test/java/ch/luschmar/jxa/auth/server/HawkAuthenticationConverterTest.java
+++ jxa-auth-server/src/test/java/ch/luschmar/jxa/auth/server/HawkAuthenticationConverterTest.java
@@ -1,9 +1,9 @@
 package ch.luschmar.jxa.auth.server;
 
 import ch.luschmar.jxa.hawk.HawkAuthenticationConverter;
-import ch.luschmar.jxa.hawk.HawkAuthenticationToken;
 import ch.luschmar.jxa.hawk.HawkKeyRepository;
 import ch.luschmar.jxa.hawk.StaticKeyRepository;
+import ch.luschmar.jxa.http.CachedBodyHttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,20 +11,24 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class HawkAuthenticationConverterTest {
     @Spy
-    HawkKeyRepository hawkKeyRepository = new StaticKeyRepository();
+    HawkKeyRepository keyRepository = new StaticKeyRepository();
+
     @InjectMocks
-    HawkAuthenticationConverter converter;
+    HawkAuthenticationConverter converter = new HawkAuthenticationConverter(keyRepository, false);
 
     /**
      * @see <a href="https://github.com/mozilla/hawk/blob/0b684f4/API.md#protocol-example">Protocol Example</a>
      */
     @Test
-    void name() {
+    void name() throws IOException {
+
         var request = new MockHttpServletRequest();
         request.setMethod("GET");
         request.setRequestURI("/resource/1");
@@ -33,9 +37,9 @@ class HawkAuthenticationConverterTest {
         request.addHeader("Authorization", "Hawk id=\"dh37fgj492je\", ts=\"1353832234\", nonce=\"j4h3g2\", ext=\"some-app-ext-data\", mac=\"6R4rV5iE+NPoym+WwjeHzjAGXUtLNIxmo1vpMofpLAE=\"");
         request.addHeader("Host", "example.com:8000");
 
-        var auth = converter.convert(request);
+        var auth = converter.convert(new CachedBodyHttpServletRequest(request));
 
-        var credentialObject = (HawkAuthenticationToken) auth.getCredentials();
-        assertEquals("dh37fgj492je", credentialObject.getCredentials());
+        var credentialObject = auth.getCredentials();
+        assertEquals("dh37fgj492je", credentialObject);
     }
 }
